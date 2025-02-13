c4ba6378bcb6381f9cf9ea8e43c6619cd1daa232
gargvinayak2005 <gargvinayak2005@gmail.com>
2025-02-13 19:59:04 +0530

    Your commit message

--- pattern/numericpalindromeequilateralpyramid.cpp
+++ pattern/numericpalindromeequilateralpyramid.cpp
@@ -1,22 +1,27 @@
-    #include<iostream>
-    using namespace std;
+#include <iostream>
+using namespace std;
 
-    int main(){
-        int n = 5;
-
-        for(int i=0;i<5;i++){
-            for(int j = 0;j < 5 - i - 1; j++){
-                cout<<"  ";
-            }
-
-            for(int j = 0; j < i + 1; j++){
-                cout << j + 1 << " ";
-            }
-
-            for(int j = 0; j < i; j++){
-                cout<< j + 1 <<" ";
-            }
-
-            cout << endl;
+int main()
+{
+    int n;
+    cin>>n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << "  ";
         }
+
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << j + 1 << " ";
+        }
+
+        for (int j = 0; j < i; j++)
+        {
+            cout << j + 1 << " ";
+        }
+
+        cout << endl;
     }
+}
