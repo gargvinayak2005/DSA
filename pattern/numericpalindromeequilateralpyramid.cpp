ac3b04a3f9471026875b50d585b5241e3ea6f88a
gargvinayak2005 <gargvinayak2005@gmail.com>
2025-02-13 19:47:24 +0530

    increased readibility

--- pattern/numericpalindromeequilateralpyramid.cpp
+++ pattern/numericpalindromeequilateralpyramid.cpp
@@ -1,22 +1,22 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    int n=5;
-    for(int i=0;i<5;i++)
-    {
-        for(int j=0;j<5-i-1;j++)
-        {
-            cout<<"  ";
+    #include<iostream>
+    using namespace std;
+
+    int main(){
+        int n = 5;
+
+        for(int i=0;i<5;i++){
+            for(int j = 0;j < 5 - i - 1; j++){
+                cout<<"  ";
+            }
+
+            for(int j = 0; j < i + 1; j++){
+                cout << j + 1 << " ";
+            }
+
+            for(int j = 0; j < i; j++){
+                cout<< j + 1 <<" ";
+            }
+
+            cout << endl;
         }
-        for(int j=0;j<i+1;j++)
-        {
-            cout<<j+1<<" ";
-        }
-        for(int j=0;j<i;j++)
-        {
-            cout<<j+1<<" ";
-        }
-        cout<<endl;
     }
-}
