e6b9ce631e3ff7046f3d3dfbe2ea31aa345c4278
gargvinayak2005 <gargvinayak2005@gmail.com>
2025-02-17 23:45:02 +0530

    new array

--- arrays/basicarrays.cpp
+++ arrays/basicarrays.cpp
@@ -1,13 +1,5 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[10];
-    //error
-    //int brr[]
-
-    int crr[]={10,20,40,70};
-    int drr[4]={0};
-    cout << drr[0] << endl;
-    int arr[4];
-    fill(arr,arr+4,101);
+    int a,
 }
